b0a9a3fafa9575a09e89ad6a5bf6b377ae8e5e37
lssjbyron <alpha.omega.666@hotmail.com>
2022-12-19 09:15:29 -0500

    fixed button

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,6 +1,6 @@
 <a
     href="{{$href}}"
-    target="$target" {{$attributes}}
+    target="$target"
     {{
         $attributes->class([
             'py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded leading-normal border inline-block transition',
--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -13,7 +13,7 @@
                     </p>
                     <ul class="flex flex-wrap items-center">
                         <li>
-                            <x-button-link href="#portfolio" variant="primary" class="rounded-lg text-orange">
+                            <x-button-link href="#portfolio" variant="primary" class="rounded-lg">
                                 My Projects
                             </x-button-link>
                         </li>
